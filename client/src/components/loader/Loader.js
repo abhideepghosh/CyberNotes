e592fad592ca0029af8ae595dd89fa46ab2f2828
Swatisharma175 <56503548+Swatisharma175@users.noreply.github.com>
2023-01-20 22:32:42 +0530

    loader change implemented

--- client/src/components/loader/Loader.js
+++ client/src/components/loader/Loader.js
@@ -1,55 +1,16 @@
-import {useState, useEffect} from 'react';
-import "./Loader.css";
+
+import "./Loader.scss";
 
 const Loader = () =>  {
-    const [word, setWord] = useState("");
-    var words = ['Loading CyberNotes','-------------->'],
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
-    speed = 90;
-  const wordflick = function () {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words[i].length) {
-        ++skip_count;
-        if (skip_count === skip_delay) {
-          forwards = false;
-          skip_count = 0;
-        }
-      }
-    }
-    else {
-      if (offset === 0) {
-        forwards = true;
-        i++;
-        offset = 0;
-        if (i >= len) {
-          i = 0;
-        }
-      }
-    }
-    setWord(words[i].substring(0, offset));
-    if (skip_count === 0) {
-      if (forwards) {
-        offset++;
-      }
-      else {
-        offset--;
-      }
-    }
-  },speed);
-};
 
-    useEffect (()=>{
-        wordflick();
-    })
 return (
-    <div id="loader">
-      <div className="word"><em>{word}</em></div>
+    <div className="loader">
+     <div class="container">
+  <div class="glitch" data-text="CyberNotes">CyberNotes</div>
+  <div class="glow">CyberNotes</div>
+  <p class="subtitle">WELCOME-TO</p>
+  </div>
+  <div class="scanlines"></div>
     </div>
   )
 }
--- client/src/components/loader/Loader2.js
+++ client/src/components/loader/Loader2.js
@@ -1,54 +1,15 @@
-import {useState, useEffect} from 'react';
-import "./Loader.css";
+
+import "./Loader.scss";
 
 const Loader2 = () =>  {
-    const [word, setWord] = useState("");
-    var words2 = ['Creating Account','-------------->'],
-    i = 0,
-    offset = 0,
-    len = words2.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
-    speed = 90;
-const wordflick = function () {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words2[i].length) {
-        ++skip_count;
-        if (skip_count === skip_delay) {
-          forwards = false;
-          skip_count = 0;
-        }
-      }
-    }
-    else {
-      if (offset === 0) {
-        forwards = true;
-        i++;
-        offset = 0;
-        if (i >= len) {
-          i = 0;
-        }
-      }
-    }
-    setWord(words2[i].substring(0, offset));
-    if (skip_count === 0) {
-      if (forwards) {
-        offset++;
-      }
-      else {
-        offset--;
-      }
-    }
-  },speed);
-};
-    useEffect (()=>{
-        wordflick();
-    })
 return (
-    <div id="loader">
-      <div className="word"><em>{word}</em></div>
+    <div className="loader">
+     <div class="container">
+  <div class="glitch" data-text="CyberNotes">CyberNotes</div>
+  <div class="glow">CyberNotes</div>
+  <p class="subtitle">REGISTERING-ON</p>
+  </div>
+  <div class="scanlines"></div>
     </div>
   )
 }
